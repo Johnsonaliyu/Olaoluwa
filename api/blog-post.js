2146901d1de56ea8a7ec8222522abaa5ee42cc5b
Aliu Johnson Temitope <140964584+Johnsonaliyu@users.noreply.github.com>
2026-04-20 15:40:26 +0100

    Update Open Graph URL for blog post

--- api/blog-post.js
+++ api/blog-post.js
@@ -56,7 +56,7 @@ module.exports = async function handler(req, res) {
   <meta property="og:image"        content="${esc(ogImage)}">
   <meta property="og:image:width"  content="1200">
   <meta property="og:image:height" content="630">
-  <meta property="og:url"          content="${SITE_URL}/blog-post.html${id ? '?id=' + encodeURIComponent(id) : ''}">
+  <meta property="og:url"          content="${SITE_URL}/blog-post-app.html${id ? '?id=' + encodeURIComponent(id) : ''}">
   <meta name="twitter:card"        content="summary_large_image">
   <meta name="twitter:title"       content="${esc(ogTitle)}">
   <meta name="twitter:description" content="${esc(ogDescription)}">
